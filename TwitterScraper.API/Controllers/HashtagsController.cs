9c5aa93d1a31bca76bbd5baf840cd1473f2e7ff3
Alan Zelaquett Hoffmann <alan.zh@hotmail.com>
2020-02-20 05:36:58 -0300

    Adicionada controller para teste e swagger

---
+++ TwitterScraper.API/Controllers/HashtagsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TwitterScraper.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class HashtagsController : Controller
+    {
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                yield return Guid.NewGuid().ToString();
+            }
+        }
+    }
+}
