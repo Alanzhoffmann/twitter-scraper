54df234b85f786e50b81e61abd270567668116bb
Alan Zelaquett Hoffmann <alan.zh@hotmail.com>
2020-02-20 07:08:05 -0300

    Adicionada controller de hashtag

--- TwitterScraper.API/Controllers/HashtagsController.cs
+++ TwitterScraper.API/Controllers/HashtagsController.cs
@@ -1,20 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace TwitterScraper.API.Controllers
 {
     [Route("api/[controller]")]
     public class HashtagsController : Controller
     {
+        private readonly AppContext _dbContext;
+
+        public HashtagsController(AppContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IEnumerable<string>> Get()
         {
-            for (int i = 0; i < 10; i++)
+            return await _dbContext.Hashtags.Select(h => h.Texto).ToListAsync();
+        }
+
+        [HttpPost("{hashtag}")]
+        public async Task<IActionResult> Save(string hashtag)
+        {
+            if (!await _dbContext.Hashtags.AnyAsync(h => h.Texto == hashtag))
             {
-                yield return Guid.NewGuid().ToString();
+                _dbContext.Hashtags.Add(new Entidades.Hashtag(hashtag));
+                await _dbContext.SaveChangesAsync();
             }
+
+            return Ok();
+        }
+
+        [HttpDelete("{hashtag}")]
+        public async Task<IActionResult> Delete(string hashtag)
+        {
+            var hashtagBanco = await _dbContext.Hashtags.FirstOrDefaultAsync(h => h.Texto == hashtag);
+            if (hashtagBanco is null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Hashtags.Remove(hashtagBanco);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok();
         }
     }
 }
